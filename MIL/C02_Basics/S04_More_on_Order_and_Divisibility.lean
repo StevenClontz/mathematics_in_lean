24091882ebbf0ca08307dcb5eee22beb95663113
Steven Clontz <steven.clontz@gmail.com>
2023-11-20 00:12:33 +0000

    2.4 through triangle inequality

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -31,7 +31,7 @@ example : min a b = min b a := by
   apply h
   apply h
 
-example : min a b = min b a := by
+theorem min_symm : min a b = min b a := by
   apply le_antisymm
   repeat
     apply le_min
@@ -39,18 +39,88 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  have h : ∀ x y : ℝ, max x y ≤ max y x := by
+    intro x y
+    apply max_le
+    apply le_max_right
+    apply le_max_left
+  apply le_antisymm
+  apply h
+  apply h
+
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  have h : ∀ x y z : ℝ, min (min x y) z ≤ min x y := by
+    intro x y z
+    apply min_le_left
+  have g : ∀ a b c : ℝ,  min (min a b) c ≤ min a (min b c) := by
+    intro a b c
+    apply le_min
+    · show min (min a b) c ≤ a
+      apply le_trans (h a b c)
+      apply min_le_left
+    · show min (min a b) c ≤ min b c
+      apply le_min
+      · show min (min a b) c ≤ b
+        rw [min_symm a b]
+        apply le_trans (h b a c)
+        apply min_le_left
+      apply min_le_right
+  apply le_antisymm
+  · show min (min a b) c ≤ min a (min b c)
+    exact g a b c
+  · show min a (min b c) ≤ min (min a b) c
+    rw [min_symm a (min b c)]
+    rw [min_symm (min a b) c]
+    rw [min_symm b c]
+    rw [min_symm a b]
+    exact g c b a
+
+
+
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  apply add_le_add_right
+  apply min_le_left
+  apply add_le_add_right
+  apply min_le_right
+
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  apply aux
+  have h: ∀ x y z : ℝ, x + -z ≤ y → x ≤ y + z := by
+    intro x y z
+    intro h
+    rw [← add_neg_cancel_right x z]
+    rw [add_comm x z, add_comm y z, add_assoc]
+    apply add_le_add_left
+    apply h
+  apply h
+  apply le_min
+  have g: min (a+c) (b+c) ≤ a+c := by
+   apply min_le_left
+  linarith [g]
+  have g: min (a+c) (b+c) ≤ b+c := by
+   apply min_le_right
+  linarith [g]
+
+
+
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
-end
+example : |a| - |b| ≤ |a - b| := by
+  have h: |a + -b + b| ≤ |a + -b| + |b| := by
+    apply abs_add
+  rw [add_assoc, add_comm (-b) b, ← add_assoc] at h
+  rw [add_neg_cancel_right, ← sub_eq_add_neg] at h
+  linarith [h]
+
+
+
+
 
 section
 variable (w x y z : ℕ)
@@ -80,5 +150,3 @@ variable (m n : ℕ)
 example : Nat.gcd m n = Nat.gcd n m := by
   sorry
 end
-
-
